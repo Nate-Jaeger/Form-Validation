3e916847727f1f2f3787e77c35baeda57f2a9493
Nate Jaeger <Nate.RJS21@gmail.com>
2020-07-03 16:27:43 -0500

    Fixed TShirt Color select menu to reflect changes in the themes section

--- js/script.js
+++ js/script.js
@@ -8,26 +8,31 @@ $('#title').on('change', e =>{
     if (e.target.value === 'other'){
         $('#other-title').show();
     }
+    else {
+        $('#other-title').hide();
+    }
 });
 
 
-//Hide T-Shirt color options until a design is picked
+//Disable the T-Shirt color options until a design is picked
 $('label[for="color"]').text("Please select a T-shirt theme");
 $('#color').attr('disabled', 'disabled');
 
 //Change event to switch between states when user is choosing between T-Shirt Themes
 $('#design').change( e => {
     let theme = e.target.value;
-    if(theme === "js puns"){
-        //Remove the disabled attr and change the text of the T-Shirt color menu
-        $('#color').removeAttr('disabled');
-        $('label[for="color"]').text("Color:");
-        //Hide the 'Select a Theme' option from the select menu
-        $('#design').children().first().hide();
+    //List of options in the select menu
+    const $options = $('#color').children();
 
-        //List of options in the select menu
-        const $options = $('#color').children();
-        //Loop through the $options array to show/hide 
+    //Remove disabled attr on color options and change the text of the T-Shirt color menu
+    $('#color').removeAttr('disabled');
+    $('label[for="color"]').text("Color:");
+    //Hide the 'Select a Theme' option from the select menu
+    $('#design').children().first().hide();
+
+    if (theme === "js puns"){
+        $('#color').val($options[0].value);
+        //Loop through the $options collection to show/hide appropriate colors
         $options.each((index, element) => {
             if (index < 3){
                 $(element).show();
@@ -38,11 +43,30 @@ $('#design').change( e => {
         });
     }
     else if (theme === "heart js"){
-        $('#color').removeAttr('disabled');
-        $('label[for="color"]').text("Color:");
-    }
-    else {
-        $('label[for="color"]').text("Please select a T-shirt theme");
-        $('#color').attr('disabled', 'disabled');
+        $('#color').val($options[3].value);
+        $options.each((index, element) => {
+            if (index > 2){
+                $(element).show();
+            }
+            else {
+                $(element).hide();
+            }
+        });
     }
 });
+
+        //User can't select events at conflicting times
+
+//Lists of checkboxes for different dates
+const $tuesNineToTwelve = $('[data-day-and-time="Tuesday 9am-12pm"]');
+const $tuesOneToFour = $('[data-day-and-time="Tuesday 1pm-4pm"]');
+const $wedNineToTwelve = $('[data-day-and-time="Wednesday 9am-12pm"]');
+const $wedOneToFour = $('[data-day-and-time="Wednesday 1pm-4pm"]');
+
+
+
+
+
+
+
+
