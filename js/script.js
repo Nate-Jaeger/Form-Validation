cf5bd421616f485b9cc853309c8d81f2c52203d7
Nate Jaeger <Nate.RJS21@gmail.com>
2020-07-03 11:14:31 -0500

    Added functionality to TShirt select menu
    If JS Puns theme is selected the TShirts for heart JS theme are hidden

--- js/script.js
+++ js/script.js
@@ -12,6 +12,34 @@ $('#title').on('change', e =>{
 
 
 //Hide T-Shirt color options until a design is picked
-if ($('#design').val() === "Select Theme"){
-    $('#color').val($('<option>Please select a T-shirt theme</option>'));
-}
+$('label[for="color"]').text("Please select a T-shirt theme");
+$('#color').attr('disabled', 'disabled');
+
+//Change event to switch between states when user is choosing between T-Shirt Themes
+$('#design').change( e => {
+    let theme = e.target.value;
+    if(theme === "js puns"){
+        //Remove the disabled attr and change the text of the T-Shirt color menu
+        $('#color').removeAttr('disabled');
+        $('label[for="color"]').text("Color:");
+
+        //List of options in the select menu
+        const $options = $('#color').children();
+        $options.each((index, element) => {
+            if (index < 3){
+                $(element).show();
+            }
+            else {
+                $(element).hide();
+            }
+        });
+    }
+    else if (theme === "heart js"){
+        $('#color').removeAttr('disabled');
+        $('label[for="color"]').text("Color:");
+    }
+    else {
+        $('label[for="color"]').text("Please select a T-shirt theme");
+        $('#color').attr('disabled', 'disabled');
+    }
+});
