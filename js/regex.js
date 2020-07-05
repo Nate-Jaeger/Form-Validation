8797cc183af7ec68a8b5e0c9d1925628ffae2b32
Nate Jaeger <Nate.RJS21@gmail.com>
2020-07-05 13:30:38 -0500

    Formatted JS files with Prettier

--- js/regex.js
+++ js/regex.js
@@ -1,85 +1,81 @@
 //Validate user's input name. Includes some special characters
 function isValidUsername() {
-    const userName = document.getElementById('name');
+  const userName = document.getElementById("name");
 
-    //Test if username is valid, else apply the invalid class styling
-    if (/^[a-z ,.'-]+$/i.test(userName.value)) {
-        userName.classList.remove("invalid");
-        return true;
-    }
-    else{
-        userName.classList.add("invalid");
-        return false;
-    }
+  //Test if username is valid, else apply the invalid class styling
+  if (/^[a-z ,.'-]+$/i.test(userName.value)) {
+    userName.classList.remove("invalid");
+    return true;
+  } else {
+    userName.classList.add("invalid");
+    return false;
+  }
 }
 
 //Validate user E-mail
 function isValidEmail() {
-    const email = document.getElementById('mail');
+  const email = document.getElementById("mail");
 
-    //Test if email is valid, or add invalid class stylings
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/i.test(email.value)) {
-        email.classList.remove("invalid");
-        return true;
-    }
-    else{
-        email.classList.add("invalid");
-        return false;
-    }
+  //Test if email is valid, or add invalid class stylings
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/i.test(email.value)) {
+    email.classList.remove("invalid");
+    return true;
+  } else {
+    email.classList.add("invalid");
+    return false;
+  }
 }
 
 //Validate if the user has selected at least 1 event
-function isEventChecked(){
-    const checkboxes = document.querySelectorAll('[type="checkbox"]');
-    let isBoxChecked = false;
+function isEventChecked() {
+  const checkboxes = document.querySelectorAll('[type="checkbox"]');
+  let isBoxChecked = false;
 
-    //Loop over all checkboxes and see if any have the checked attribute
-    checkboxes.forEach( checkbox => {
-        //If they do, change isBoxChecked to true
-        if(checkbox.checked){
-            isBoxChecked = true;
-        }
-    });
-    
-    return isBoxChecked;
+  //Loop over all checkboxes and see if any have the checked attribute
+  checkboxes.forEach((checkbox) => {
+    //If they do, change isBoxChecked to true
+    if (checkbox.checked) {
+      isBoxChecked = true;
+    }
+  });
+
+  return isBoxChecked;
 }
 
 //Validate that user's CC information is correct
-function isValidCard(){
-    //Grab values from all CC input fields
-    const cardNum = document.getElementById('cc-num');
-    const zipCode = document.getElementById('zip');
-    const CVV = document.getElementById('cvv');
+function isValidCard() {
+  //Grab values from all CC input fields
+  const cardNum = document.getElementById("cc-num");
+  const zipCode = document.getElementById("zip");
+  const CVV = document.getElementById("cvv");
 
-    //Check if CC payment option is selected before validating inputs
-    if ($('#payment').val() === "credit card") {
-        //Check if each CC info input is failing regex test, if it is, function will return false
-        if (!(/^\d{13,16}$/.test(cardNum.value))) {
-            return false;
-        }
-        else if (!(/^\d{5}$/.test(zipCode.value))) {
-            return false;
-        }
-        else if (!(/^\d{3}$/.test(CVV.value))) {
-            return false;
-        }
-    } 
-    
-    //If none of the credentials failed and returned false, true will be returned
-    return true;
+  //Check if CC payment option is selected before validating inputs
+  if ($("#payment").val() === "credit card") {
+    //Check if each CC info input is failing regex test, if it is, function will return false
+    if (!/^\d{13,16}$/.test(cardNum.value)) {
+      return false;
+    } else if (!/^\d{5}$/.test(zipCode.value)) {
+      return false;
+    } else if (!/^\d{3}$/.test(CVV.value)) {
+      return false;
+    }
+  }
+
+  //If none of the credentials failed and returned false, true will be returned
+  return true;
 }
 
 //Function to call all other validation functions
-function validateInputs(){
-    //Check if all input validations return true
-    if(isValidUsername()){
-        if(isValidEmail()){
-            if(isEventChecked()){
-                if(isValidCard()){
-                     return true;
-                 }
-            }
+function validateInputs() {
+  //Check if all input validations return true
+  if (isValidUsername()) {
+    if (isValidEmail()) {
+      if (isEventChecked()) {
+        if (isValidCard()) {
+          return true;
         }
+      }
     }
-    return false;
+  }
+  return false;
 }
--- js/script.js
+++ js/script.js
@@ -1,159 +1,148 @@
 //Set focus on Name input on page load
-$('#name').focus();
+$("#name").focus();
 
 //Hide the 'Other Role' input until the user selects the other option
-$('#other-title').hide();
+$("#other-title").hide();
 
-$('#title').on('change', e =>{
-    if (e.target.value === 'other'){
-        $('#other-title').show();
-    }
-    else {
-        $('#other-title').hide();
-    }
+$("#title").on("change", (e) => {
+  if (e.target.value === "other") {
+    $("#other-title").show();
+  } else {
+    $("#other-title").hide();
+  }
 });
-
 
 //Disable the T-Shirt color options until a design is picked
 $('label[for="color"]').text("Please select a T-shirt theme");
-$('#color').attr('disabled', true);
-$('#color').val("");
+$("#color").attr("disabled", true);
+$("#color").val("");
 
 //Change event to switch between states when user is choosing between T-Shirt Themes
-$('#design').change( e => {
-    let theme = e.target.value;
-    //List of options in the select menu
-    const $options = $('#color').children();
+$("#design").change((e) => {
+  let theme = e.target.value;
+  //List of options in the select menu
+  const $options = $("#color").children();
 
-    //Remove disabled attr on color options and change the text of the T-Shirt color menu
-    $('#color').removeAttr('disabled');
-    $('label[for="color"]').text("Color:");
-    //Hide the 'Select a Theme' option from the select menu after a theme selection has been made
-    $('#design').children().first().hide();
+  //Remove disabled attr on color options and change the text of the T-Shirt color menu
+  $("#color").removeAttr("disabled");
+  $('label[for="color"]').text("Color:");
+  //Hide the 'Select a Theme' option from the select menu after a theme selection has been made
+  $("#design").children().first().hide();
 
-    if (theme === "js puns"){
-        $('#color').val($options[0].value);
-        //Loop through the $options collection to show/hide appropriate colors
-        $options.each((index, element) => {
-            if (index < 3){
-                $(element).show();
-            }
-            else {
-                $(element).hide();
-            }
-        });
-    }
-    else if (theme === "heart js"){
-        $('#color').val($options[3].value);
-        $options.each((index, element) => {
-            if (index > 2){
-                $(element).show();
-            }
-            else {
-                $(element).hide();
-            }
-        });
-    }
+  if (theme === "js puns") {
+    $("#color").val($options[0].value);
+    //Loop through the $options collection to show/hide appropriate colors
+    $options.each((index, element) => {
+      if (index < 3) {
+        $(element).show();
+      } else {
+        $(element).hide();
+      }
+    });
+  } else if (theme === "heart js") {
+    $("#color").val($options[3].value);
+    $options.each((index, element) => {
+      if (index > 2) {
+        $(element).show();
+      } else {
+        $(element).hide();
+      }
+    });
+  }
 });
 
-
-    //Create/Append HTML element to display Total
+//Create/Append HTML element to display Total
 let totalCost = 0;
 const totalP = $(`<p id="total">Total: $0 </p>`);
-$('.activities').append(totalP);
+$(".activities").append(totalP);
 
-    //Change event on activities section
-$('.activities').change( e => {
-    const activitiesCollection = $('.activities input');
-    const activity = e.target;
-    const $cost = parseInt($(e.target).attr('data-cost'));
-    const $time = $(e.target).attr('data-day-and-time');
-    
-    //Add or Subtract the cost of the course that was just checked/unchecked
-    if (activity.checked){
-        totalCost += $cost;
-        totalP.text(`Total: $${totalCost}`);
-    } else {
-        totalCost -= $cost;
-        totalP.text(`Total: $${totalCost}`);
+//Change event on activities section
+$(".activities").change((e) => {
+  const activitiesCollection = $(".activities input");
+  const activity = e.target;
+  const $cost = parseInt($(e.target).attr("data-cost"));
+  const $time = $(e.target).attr("data-day-and-time");
+
+  //Add or Subtract the cost of the course that was just checked/unchecked
+  if (activity.checked) {
+    totalCost += $cost;
+    totalP.text(`Total: $${totalCost}`);
+  } else {
+    totalCost -= $cost;
+    totalP.text(`Total: $${totalCost}`);
+  }
+
+  //Disable activities that have conflicting scheduals
+  for (let i = 0; i < activitiesCollection.length; i++) {
+    let activityTime = $(activitiesCollection[i]).attr("data-day-and-time");
+
+    if (activityTime === $time && activitiesCollection[i] !== activity) {
+      if (activity.checked) {
+        //Disable conflicting workshops
+        $(activitiesCollection[i]).attr("disabled", true);
+        $(activitiesCollection[i]).parent().addClass("disabled");
+      } else {
+        //Enable clicked checkbox and remove the disabled class from its label
+        $(activitiesCollection[i]).attr("disabled", false);
+        $(activitiesCollection[i]).parent().removeClass("disabled");
+      }
     }
-
-
-    //Disable activities that have conflicting scheduals
-    for (let i = 0; i < activitiesCollection.length; i++){
-        let activityTime = $(activitiesCollection[i]).attr('data-day-and-time');
-
-        if(activityTime === $time && activitiesCollection[i] !== activity){
-            if (activity.checked){
-                //Disable conflicting workshops
-             $(activitiesCollection[i]).attr('disabled', true);
-             $(activitiesCollection[i]).parent().addClass("disabled");
-            } 
-            else {
-                //Enable clicked checkbox and remove the disabled class from its label
-                $(activitiesCollection[i]).attr('disabled', false);
-                $(activitiesCollection[i]).parent().removeClass("disabled");
-            }
-        }
-        
-    }
+  }
 });
 
-
-    //Hide the "Select Payment Method" in the payment dropdown and make CC the selected option
+//Hide the "Select Payment Method" in the payment dropdown and make CC the selected option
 $('#payment option[value="select method"]').hide();
-$('#payment option[value="credit card"]').attr('selected', true);
+$('#payment option[value="credit card"]').attr("selected", true);
 //Hide all other payment divs since CC will be the one to display automaticlly
-$('#paypal').hide();
-$('#bitcoin').hide();
+$("#paypal").hide();
+$("#bitcoin").hide();
 
 //Change event listener on the payment select menu
-$('#payment').change(e => {
-    const paymentOption = $('#payment').val();
-    //Payment object to show/hide appropriate HTML depending on what payment method was selected
-    const payment = {
-        creditCard: () => {
-            $('#credit-card').show();
-            $('#paypal').hide();
-            $('#bitcoin').hide();
-        },
-        paypal: () => {
-            $('#paypal').show();
-            $('#credit-card').hide();
-            $('#bitcoin').hide();
-        },
-        bitcoin: () => {
-            $('#bitcoin').show();
-            $('#credit-card').hide();
-            $('#paypal').hide();
-        }
-    };
+$("#payment").change((e) => {
+  const paymentOption = $("#payment").val();
+  //Payment object to show/hide appropriate HTML depending on what payment method was selected
+  const payment = {
+    creditCard: () => {
+      $("#credit-card").show();
+      $("#paypal").hide();
+      $("#bitcoin").hide();
+    },
+    paypal: () => {
+      $("#paypal").show();
+      $("#credit-card").hide();
+      $("#bitcoin").hide();
+    },
+    bitcoin: () => {
+      $("#bitcoin").show();
+      $("#credit-card").hide();
+      $("#paypal").hide();
+    },
+  };
 
-    //Switch statement to determine which payment HTML element needs to be displayed
-    switch (paymentOption){
-        case "credit card":
-            payment.creditCard();
-            break;
-        case "paypal":
-            payment.paypal();
-            break;
-        case "bitcoin":
-            payment.bitcoin();
-            break;
-        default:
-            break;
-    }
+  //Switch statement to determine which payment HTML element needs to be displayed
+  switch (paymentOption) {
+    case "credit card":
+      payment.creditCard();
+      break;
+    case "paypal":
+      payment.paypal();
+      break;
+    case "bitcoin":
+      payment.bitcoin();
+      break;
+    default:
+      break;
+  }
 });
 
 //Listen for a click event on the submit button
-$('[type="submit"]').click( (e) => {
-    //Check if all input validations return true
-    if (validateInputs()) { 
-        e.preventDefault();
-        console.log("Valid Inputs");
-    }
-    else{
-        e.preventDefault();
-        console.log("Invalid Inputs");
-    }
+$('[type="submit"]').click((e) => {
+  //Check if all input validations return true
+  if (validateInputs()) {
+    e.preventDefault();
+    console.log("Valid Inputs");
+  } else {
+    e.preventDefault();
+    console.log("Invalid Inputs");
+  }
 });
