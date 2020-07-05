7a4cae68fc565a9a62d8614e14bcebc305420e16
Nate Jaeger <Nate.RJS21@gmail.com>
2020-07-04 23:54:01 -0500

    Added validation for User's name and Email

--- js/regex.js
+++ js/regex.js
@@ -1,6 +1,14 @@
-//Validate user's input name. 
+//Global input variables
+const userName = document.getElementById('name').value;
+
+//Validate user's input name. Includes some special characters
 function isValidUsername(username) {
     return /^[a-z ,.'-]+$/i.test(username);
 }
 
-console.log(isValidUsername("Nate Jaeger-Smith"));
+//Validate user E-mail
+function isValidEmail(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i.test(email);
+}
+
+console.log(isValidEmail("Nate.RJS21@gmail.com"));
