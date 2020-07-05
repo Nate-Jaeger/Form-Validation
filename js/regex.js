04ebc081b1c1266a7c566d9a089cf1f3670f2844
Nate Jaeger <Nate.RJS21@gmail.com>
2020-07-05 00:46:10 -0500

    Added validation code for selecting atleast 1 event

--- js/regex.js
+++ js/regex.js
@@ -11,4 +11,24 @@ function isValidEmail(email) {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i.test(email);
 }
 
-console.log(isValidEmail("Nate.RJS21@gmail.com"));
+//Validate if the user has selected at least 1 event event
+function isEventChecked(){
+    const checkboxes = document.querySelectorAll('[type="checkbox"]');
+    let isBoxChecked = false;
+
+    //Loop over all checkboxes and see if any have the checked attribute
+    checkboxes.forEach( checkbox => {
+        //If they do, change isBoxChecked to true
+        if(checkbox.checked){
+            isBoxChecked = true;
+        }
+    });
+    
+    //If a checkbox has been checked, return true, if not return false
+    if(isBoxChecked){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
